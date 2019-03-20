2073f3b261db8b056ffeb435870a7a2aa478661b
nikai <keni1006@163.com>
2019-03-21 01:55:40 +0800

    http://zookeeper.apache.org/

--- src/main/java/com/nikai/distributed/lock/redis/RedisLock.java
+++ src/main/java/com/nikai/distributed/lock/redis/RedisLock.java
@@ -1,5 +1,7 @@
 package com.nikai.distributed.lock.redis;
 
+import java.util.Collections;
+
 /**
  * distributed-lock com.nikai.distributed.lock.redis
  *
@@ -9,5 +11,24 @@ package com.nikai.distributed.lock.redis;
  * @Modified By:
  */
 public class RedisLock {
+    public static final String lua = "if redis.call('get', KEYS[1]) == ARGV[1]  then   return redis.call('del', KEYS[1]) else return 0 end";
 
+    public static boolean tryLock(final String key, final String value) {
+        while (true) {
+            Long lock = JedisUtils.setnx(key, value);
+            if (lock == 1) {
+                return true;
+            }
+        }
+    }
+
+    public static void lock(final String key, final String value, final int cacheSeconds) {
+        if (tryLock(key, value)) {
+            JedisUtils.setexpire(key, value, cacheSeconds);
+        }
+    }
+
+    public static void unlock(final String key, final String value) {
+        JedisUtils.eval(lua, Collections.singletonList(key), Collections.singletonList(value));
+    }
 }
